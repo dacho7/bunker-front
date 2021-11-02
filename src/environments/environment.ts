a00c2e043a0374e560a22b6332b000b6467d8096
Dacho7 <davidtobarst@gmail.com>
2021-11-02 17:03:36 -0500

    change acount in firebase for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAw1AhQqe3iYzIikGZxMwxwZk4ewTd7WBU",
-    authDomain: "dbbnk-99024.firebaseapp.com",
-    projectId: "dbbnk-99024",
-    storageBucket: "dbbnk-99024.appspot.com",
-    messagingSenderId: "418025800953",
-    appId: "1:418025800953:web:f18d6d03bf2c8ac168ce28"
+    apiKey: "AIzaSyB5F_1gzS1k9DDYFg3bqRgeKDfILN8iI1o",
+    authDomain: "bnkdb-fda9b.firebaseapp.com",
+    databaseURL: "https://bnkdb-fda9b-default-rtdb.firebaseio.com",
+    projectId: "bnkdb-fda9b",
+    storageBucket: "bnkdb-fda9b.appspot.com",
+    messagingSenderId: "808106209561",
+    appId: "1:808106209561:web:e0352a376b9f4d0101d588"
   }
 };
 
