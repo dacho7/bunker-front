91a94efb4ff0d7e61db599d409ffee12782f4428
Dacho7 <davidtobarst@gmail.com>
2021-11-07 01:40:06 -0500

    sales interfaces

--- src/app/sales/registersales/registersales.component.ts
+++ src/app/sales/registersales/registersales.component.ts
@@ -3,13 +3,109 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registersales',
   templateUrl: './registersales.component.html',
-  styleUrls: ['./registersales.component.css']
+  styleUrls: ['./registersales.component.css'],
 })
 export class RegistersalesComponent implements OnInit {
+  name!: string;
+  product!: string;
+  quantity = 1;
+  payMethod = 'Efectivo';
+  phone!: string;
+  address!: string;
+  email!: string;
+  price = 2000;
 
-  constructor() { }
+  salesAvailables: Array<any> = [];
+
+  constructor(private _saleService: SaleService) {}
 
   ngOnInit(): void {
+    this._saleService.lisOnlyNotSend().subscribe((doc) => {
+      this.salesAvailables = [];
+      let salesAux: any[] = [];
+      doc.forEach((element: any) => {
+        const sale: Sale = {
+          id: element.payload.doc.id,
+          ...element.payload.doc.data(),
+        };
+        salesAux.push(sale);
+      });
+      //      console.log(salesAux.sort((a: Sale,b: Sale) => new Date(a.dateCreated).getTime() > new Date(b.dateCreated).getTime()));
+      this.salesAvailables = salesAux.sort((a, b): any => {
+        if (a.dateCreated > b.dateCreated) {
+          return 1;
+        }
+        if (a.dateCreated < b.dateCreated) {
+          return -1;
+        }
+      });
+    });
   }
 
+  registerSale() {
+    if (!this.validateSale()) {
+      return;
+    } else {
+      if (!this.phone) {
+        this.phone = '';
+      }
+      if (!this.address) {
+        this.address = '';
+      }
+      if (!this.email) {
+        this.email = '';
+      }
+    }
+    const sale: Sale = {
+      client: this.name,
+      product: this.product,
+      quantity: this.quantity,
+      payMethod: this.payMethod,
+      phone: this.phone,
+      address: this.address,
+      email: this.email,
+      state: 'en preparacion',
+      dateCreated: new Date(),
+      price: this.price,
+    };
+    this._saleService
+      .saveSale(sale)
+      .then(() => this.resetFields())
+      .catch((e) => console.log(e));
+  }
+
+  validateSale() {
+    if (!this.name || !this.product || !this.quantity || !this.address) {
+      alert('Digite todos los campos');
+      return false;
+    }
+    if (
+      this.payMethod === 'Efectivo' ||
+      this.payMethod === 'Nequi' ||
+      this.payMethod === 'Daviplata' ||
+      this.payMethod === 'Tarjeta'
+    ) {
+      return true;
+    } else {
+      alert('Ingrese un metodo de pago valido (Efectivo, Nequi, Daviplata)');
+      return false;
+    }
+  }
+
+  sendOrder(id: string) {
+    this._saleService
+      .sendOrder(id)
+      .then()
+      .catch((err) => console.log(err));
+  }
+
+  resetFields() {
+    this.name = '';
+    this.product = '';
+    this.quantity = 1;
+    this.phone = '';
+    this.address = '';
+    this.email = '';
+    this.payMethod = 'Efectivo';
+  }
 }
